e857a08abbaa362a6715e013c9bfecdbff5de833
Na4xo <davidsatel033@gmail.com>
2023-11-08 07:58:33 +0300

    Create 1234.py

---
+++ 1234.py
@@ -0,0 +1,53 @@
+
+# string = input("Введите строку из двух слов, разделенных пробелом: ")
+# words = string.split()
+
+# result = " ".join(reversed(words))
+
+
+# print("Результат:", result)
+
+
+# string = input("Введите строку: ")
+# first_index_f = string.find("f")
+# if first_index_f != -1:
+#     if string.count("f") == 1:
+#         print("Индекс символа f:", first_index_f)
+#     else:
+#         last_index_f = first_index_f
+#         for i in range(first_index_f + 1, len(string)):
+#             if string[i] == "f":
+#                 last_index_f = i
+#         print("Индексы символа f:", first_index_f, last_index_f)
+
+
+
+
+
+# s = input()
+# first_index = s.find('f')
+
+
+# if first_index == -1:
+#     print(-2)
+# else:
+   
+#     second_index = s.find('f', first_index + 1)
+
+#     if second_index == -1:
+#         print(-1)
+#     else:
+#         print(second_index)
+
+
+# s = input()  
+
+
+# first_h = s.find('h')
+# last_h = s.rfind('h')
+
+
+# result = s[:first_h] + s[last_h+1:]
+
+# print(result)  
+
